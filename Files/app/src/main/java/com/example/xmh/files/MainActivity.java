1bb207191fb4678ac90e0347874cf3fa9b863a01
osjn <osjn@outlook.com>
2016-03-02 08:42:37 +0800

    add

--- Files/app/src/main/java/com/example/xmh/files/MainActivity.java
+++ Files/app/src/main/java/com/example/xmh/files/MainActivity.java
@@ -5,12 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
     EditText textBox;
@@ -38,8 +44,42 @@ public class MainActivity extends AppCompatActivity {
     public void onClickSave(View view) {
         String str = textBox.getText().toString();
         try {
-            FileOutputStream fOut =
-                    openFileOutput("textfile.txt", Mode_)
+            File outFile = new File("textfile.txt");
+            FileOutputStream fOut = new FileOutputStream(outFile);
+            OutputStreamWriter osw = new OutputStreamWriter(fOut);
+            osw.write(str);
+            osw.flush();
+            osw.close();
+
+            Toast.makeText(getBaseContext(), "File saved successfully!", Toast.LENGTH_SHORT).show();
+
+            textBox.setText("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void onClickLoad(View view) {
+        try {
+            FileInputStream fIn = openFileInput("textfile.txt");
+            InputStreamReader isr = new InputStreamReader(fIn);
+
+            char[] inputBuffer = new char[READ_BLOCK_SIZE];
+            String s = "";
+
+            int charRead;
+            while ((charRead = isr.read(inputBuffer)) > 0) {
+                String readString = String.copyValueOf(inputBuffer, 0, charRead);
+                s += readString;
+
+                inputBuffer = new char[READ_BLOCK_SIZE];
+            }
+
+            textBox.setText(s);
+
+            Toast.makeText(getBaseContext(), "File loaded successfully!", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
