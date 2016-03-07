e3a43aac0aa2bcace050e7c38cf2084c3d9d8cf1
osjn <osjn@outlook.com>
2016-03-07 13:35:26 +0800

    update

--- Networking/app/src/main/java/com/leclex/networking/MainActivity.java
+++ Networking/app/src/main/java/com/leclex/networking/MainActivity.java
@@ -9,11 +9,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -21,6 +26,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class MainActivity extends AppCompatActivity {
     ImageView img;
@@ -52,6 +61,64 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return in;
+    }
+
+    private String WordDefinition(String word) {
+        InputStream in;
+        String strDefinition = "";
+        try {
+            in = OpenHttpConnection("http://services.aonaware.com/DictService/DictService.asmx/Define?word=" + word);
+            Document doc = null;
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db;
+            try {
+                db = dbf.newDocumentBuilder();
+                doc = db.parse(in);
+            } catch (ParserConfigurationException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            doc.getDocumentElement().normalize();
+
+            // retrieve all the <Definition> elements
+            NodeList definitionElements = doc.getElementsByTagName("Definition");
+            // iterate through each <Definition> elements
+            for (int i = 0; i < definitionElements.getLength(); i++) {
+                Node itemNode = definitionElements.item(i);
+                if (itemNode.getNodeType() == Node.ELEMENT_NODE) {
+                    // convert the Definition node into an Element
+                    Element definitionElement = (Element) itemNode;
+                    // get all the <WordDefinition> elements under the <Definition> element
+                    NodeList wordDefinitionElements = (definitionElement).getElementsByTagName("WordDefinition");
+                    strDefinition = "";
+                    // iterate through each <WordDefinition> elements
+                    for (int j = 0; j < wordDefinitionElements.getLength(); j++) {
+                        // convert a <WordDefinition> node into an Element
+                        Element wordDefinitionElement = (Element) wordDefinitionElements.item(j);
+
+                        // get all the child nodes under the <WordDefinition> element
+                        NodeList textNodes = ((Node) wordDefinitionElement).getChildNodes();
+                        strDefinition += ((Node) textNodes.item(0)).getNodeValue() + ". \n";
+                    }
+                }
+            }
+        } catch (IOException e) {
+            Log.d("NetworkingActivity", e.getLocalizedMessage());
+        }
+
+        // return the definition of the word
+        return strDefinition;
+    }
+
+    private class AccessWebServiceTask extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... urls) {
+            return WordDefinition(urls[0]);
+        }
+
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), result, Toast.LENGTH_SHORT).show();
+        }
     }
 
     private Bitmap DownloadImage(String URL) {
@@ -187,9 +254,10 @@ public class MainActivity extends AppCompatActivity {
 //        );
 
         // download text
-        new DownloadTextTask().execute(
-            "http://www.bing.com"
-        );
+//        new DownloadTextTask().execute("http://www.bing.com");
+
+        // access a Web Service using GET
+        new AccessWebServiceTask().execute("apple");
     }
 
     @Override
