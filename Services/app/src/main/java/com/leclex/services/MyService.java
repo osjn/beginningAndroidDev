f09a69bbbc24a8aae7977a3d16868dcd0367890b
osjn <osjn@outlook.com>
2016-03-07 18:25:16 +0800

    add

---
+++ Services/app/src/main/java/com/leclex/services/MyService.java
@@ -0,0 +1,79 @@
+package com.leclex.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // We want this service to continue running until it is explicitly
+        // stopped, so return sticky.
+//        Toast.makeText(this, "Service Started", Toast.LENGTH_SHORT).show();
+
+        try {
+//            int result = DownloadFile(new URL("http://www.amazon.com/somefile.pdf"));
+//            Toast.makeText(getBaseContext(), "Downloaded " + result + "bytes", Toast.LENGTH_SHORT).show();
+            new DoBackgroundTask().execute(
+                    new URL("http://www.amazon.com/somefile.pdf"),
+                    new URL("http://www.wrox.com/somefile.pdf"),
+                    new URL("http://www.google.com/somefile.pdf"),
+                    new URL("http://www.leclex.com/somefile.pdf"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return START_STICKY;
+    }
+
+    private int DownloadFile(URL url) {
+        try {
+            // simulate taking some time to download a file
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return 100;
+    }
+
+    private class DoBackgroundTask extends AsyncTask<URL, Integer, Long> {
+        protected Long doInBackground(URL... urls) {
+            int count = urls.length;
+            long totalBytesDownloaded = 0;
+            for (int i = 0; i < count; i++) {
+                totalBytesDownloaded += DownloadFile(urls[i]);
+                //calculate percentage downloaded and report its progress
+                publishProgress((int) (((i + 1) / (float) count) * 100));
+            }
+
+            return totalBytesDownloaded;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            Log.d("Downloading files", String.valueOf(progress[0]) + "% downloaded");
+            Toast.makeText(getBaseContext(), String.valueOf(progress[0]) + "% downloaded", Toast.LENGTH_SHORT).show();
+        }
+
+        protected void onPostExecute(Long result) {
+            Toast.makeText(getBaseContext(), "Downloaded " + result + " bytes", Toast.LENGTH_SHORT).show();
+            stopSelf();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_SHORT).show();
+    }
+}
